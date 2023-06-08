d33545c4683c8a69a105f0bd9ce5be32ef8d52c0
Mustafa Dursun <dursunmustafainfo@gmail.com>
2023-06-08 18:15:03 +0300

    tamam

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const container = document.querySelector('.container');
+const loading = document.querySelector('.loading');
+const gallery = document.querySelector('.gallery');
 const urlLorem = 'https://picsum.photos/';
 
 
 const rows =30;
-
 for (let i = 0; i < rows; i++) {
     const img = document.createElement("img");
     img.src = `${urlLorem}${getRandomSize()}`
     container.appendChild(img);
-    console.log(img);
-    
 };
 
 function getRandomNumber(){
     return Math.floor(Math.random()*10) +300;
 }
-console.log(getRandomNumber());
 
 function getRandomSize(){
     return `${getRandomNumber()}/${getRandomNumber()}`
 }
-console.log(getRandomSize());
+
+
+window.addEventListener('load', () => {
+    loading.style.display = 'none';
+    gallery.style.display = 'block';
+})
+ 
